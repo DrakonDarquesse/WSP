de803bd56f6c7da35bfde482db1d6df3e4018b09
DrakonDarquesse <mingliangzheng17@gmail.com>
2022-03-02 06:27:58 +0800

    add rolelist member into Member

--- lib/models/member.dart
+++ lib/models/member.dart
@@ -1,12 +1,17 @@
+import 'package:app/models/role.dart';
+
 class Member {
   String email;
   String password;
   String name;
   bool isActive;
+  List<Role> roles;
 
-  Member(
-      {required this.email,
-      required this.password,
-      required this.name,
-      this.isActive = true});
+  Member({
+    required this.email,
+    required this.password,
+    required this.name,
+    this.isActive = true,
+    this.roles = const [],
+  });
 }
