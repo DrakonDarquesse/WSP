0c689140ca53bca2a12664fa9d8d27b9efcfe7b5
DrakonDarquesse <mingliangzheng17@gmail.com>
2022-03-07 02:36:24 +0800

    tweak button widget

--- lib/widgets/button_widget.dart
+++ lib/widgets/button_widget.dart
@@ -4,19 +4,42 @@ import 'package:app/utils/colours.dart';
 class ButtonWidget extends StatelessWidget {
   final String text;
   final VoidCallback callback;
+  final IconData? icon;
 
-  const ButtonWidget({Key? key, required this.text, required this.callback})
-      : super(key: key);
+  const ButtonWidget({
+    Key? key,
+    required this.text,
+    required this.callback,
+    this.icon,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: MaterialButton(
         onPressed: callback,
-        child: Text(text),
+        child: RichText(
+          text: TextSpan(
+            style: const TextStyle(color: Colors.white),
+            children: icon != null
+                ? [
+                    WidgetSpan(
+                        child: Padding(
+                      padding: const EdgeInsets.only(right: 5),
+                      child: Icon(
+                        icon,
+                        color: Colors.white,
+                        size: 16,
+                      ),
+                    )),
+                    TextSpan(text: text)
+                  ]
+                : [TextSpan(text: text)],
+          ),
+        ),
         padding: const EdgeInsets.all(20),
         color: blue(),
-        textColor: Colors.white,
+        minWidth: 0,
       ),
       padding: const EdgeInsets.all(16),
     );
